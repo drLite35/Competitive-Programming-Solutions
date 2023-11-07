055c87cfad5da4f88a24af6275f33904bb899273
Anurag Singh <principalquantum30@gmail.com>
2023-11-07 21:15:49 +0530

    CodeForces #1772A : Implementation.

---
+++ Codeforces/1772A-A+B?.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+    string s;
+    cin>>s;
+    int tot=0;
+    int a = (int)s[0];
+    int b = (int)s[2];
+    cout<<a+b-96<<endl;}
+    return 0;
+}
