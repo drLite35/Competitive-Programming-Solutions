e14ec0d366141945e29fb28a6357d034c314ab45
Anurag Singh <principalquantum30@gmail.com>
2023-11-11 19:15:14 +0530

    CodeForces #1873A : Implementation.

---
+++ Codeforces/1873A-Short-Sort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+    string s;
+    cin>>s;
+    if(s=="bca" || s=="cab" )cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+    }
+    return 0;
+}
