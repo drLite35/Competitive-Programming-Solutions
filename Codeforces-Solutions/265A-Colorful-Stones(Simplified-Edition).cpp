b6a30b4a8692ae5b7136a04ef9dc40076dde6678
Anurag Singh <principalquantum30@gmail.com>
2023-11-21 13:51:06 +0530

    CodeForces #265A : String Implementation.

---
+++ Codeforces-Solutions/265A-Colorful-Stones(Simplified-Edition).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>         
+#include <queue>
+#include <stack>
+#include <algorithm>
+using namespace std;
+#define ll long long
+#define ld long double
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    string s ,t;
+    cin>>s>>t;
+    int ans=1;
+    for(int i=0 ; i<t.size() ; i++){
+    	if(s[ans-1]==t[i])ans++;
+    }
+    cout<<ans;
+    return 0;
+
+}
