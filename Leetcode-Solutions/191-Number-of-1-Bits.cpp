730ca542665f1c4a119f37eccfe0b93b3b26adc4
Anurag Singh <principalquantum30@gmail.com>
2023-11-29 18:11:44 +0530

    LeetCode #191 : Bit-Shifting.

---
+++ Leetcode-Solutions/191-Number-of-1-Bits.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int cnt=0;
+        while(n>0){
+            if(n&1==1)cnt++;
+            n>>=1;
+        }
+        return cnt;
+    }
+};
