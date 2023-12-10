a45776288e936c9130a62e4b55992a5180da857c
Anurag Singh <principalquantum30@gmail.com>
2023-12-10 11:51:41 +0530

    GFG #Medium : Hash Sum Implementation.

---
+++ GeeksForGeeks-Solutions/73-Subarray-with-0-sum.cpp
@@ -0,0 +1,18 @@
+#include<map>
+using namespace std;
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        // unordered_set<int> s;
+        // int sum=0;
+        // for(int i=0 ; i<n ; i++){
+        //     sum+=arr[i];
+        //     if(sum==0 || s.find(sum)!=s.end())return 1;
+        //     s.insert(sum);
+        // }
+        return 0;
+    }
+};
