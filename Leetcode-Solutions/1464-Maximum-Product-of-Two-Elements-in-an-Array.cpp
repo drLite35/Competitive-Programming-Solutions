f75165ae5dc7530bd225b7fbcd65df72d3b4c17d
Anurag Singh <principalquantum30@gmail.com>
2023-12-12 14:49:51 +0530

    LeetCode #1464 : Sorting and Max Product.

---
+++ Leetcode-Solutions/1464-Maximum-Product-of-Two-Elements-in-an-Array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        sort(nums.begin() , nums.end() , greater<int>());
+        return (nums[0]-1)*(nums[1]-1);
+    }
+};
