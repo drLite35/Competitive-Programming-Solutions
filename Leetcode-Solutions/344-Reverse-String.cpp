c0609f734cb62091f1079cedf6ef829e36c5deb8
Anurag Singh <principalquantum30@gmail.com>
2024-02-21 19:16:21 +0530

    LeetCode #344 : Reverse the String Implementation.
    LeetCode #88 : Merge Sorted Array.

---
+++ Leetcode-Solutions/344-Reverse-String.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0;
+        int j = s.size()-1;
+        while(i<j){
+            char temp = s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;j--;
+        }
+    }
+};
