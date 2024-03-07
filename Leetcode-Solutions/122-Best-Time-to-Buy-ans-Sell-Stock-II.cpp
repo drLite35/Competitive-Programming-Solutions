58c75cbc53791fd155a6ce46292e7c44d7b56493
Anurag Singh <principalquantum30@gmail.com>
2024-03-07 15:46:26 +0530

    LeetCode #122 Buy Sell II

---
+++ Leetcode-Solutions/122-Best-Time-to-Buy-ans-Sell-Stock-II.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+  int maxProfit(vector<int> &prices) {
+    int ans = 0;
+    for (int i = 0; i < prices.size() - 1; i++) {
+      if (prices[i + 1] > prices[i]) {
+        ans += prices[i + 1] - prices[i];
+      }
+    }
+    return ans;
+  }
+};
