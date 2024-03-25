d7ccc2b1f03d03326b35171f40d0249b7a73d05d
Anurag Singh <principalquantum30@gmail.com>
2024-03-26 04:17:59 +0530

    LeetCode #442 : Duplicates without using extra space and O(n) by                 using each index as a flag as numbers are from 1 to n.

---
+++ Leetcode-Solutions/442-Find-all-Duplicates-in-an-array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+  vector<int> findDuplicates(vector<int> &nums) {
+    vector<int> ans;
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[abs(nums[i]) - 1] < 0)
+        ans.push_back(abs(nums[i]));
+      nums[abs(nums[i]) - 1] *= -1;
+    }
+    return ans;
+  }
+};
